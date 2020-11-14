f6c4ad3cf86135cac575e0db17e1e021944fe2e7
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-14 13:26:40 +0100

    Add arrays with numbers, operations and ids

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
 import "../styleSheets/App.css";
 import React from "react";
 
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const operations = ["/", "*", "-", "+"];
+const ids = {
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  4: "four",
+  5: "five",
+  6: "six",
+  1: "one",
+  2: "two",
+  3: "three",
+  0: "zero",
+  "/": "divide",
+  "*": "multiply",
+  "-": "subtract",
+  "+": "add",
+};
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastPressed: undefined,
+      calc: "0",
+      operation: undefined,
+    };
+  }
   render() {
     return (
       <div className="calculator">
         <div id="display" className="display">
-          display
+          {this.state.calc}
         </div>
         <div className="nums-container">
           <button className="ac big-h" onClick={this.handleClick} id="clear">
